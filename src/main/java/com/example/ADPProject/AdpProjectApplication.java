773f7bf108a6543dce8cfddc7fb094832ec7a7a6
wasadmin <wasadmin@ip-172-31-40-29.ap-south-1.compute.internal>
2024-10-14 08:39:39 +0000

    Initial commit

---
+++ src/main/java/com/example/ADPProject/AdpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ADPProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ADPProject/AdpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ADPProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
